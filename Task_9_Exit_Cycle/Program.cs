165271fdacd98a810f478973cc315c825091369c
whiskas009 <76867818+whiskas009@users.noreply.github.com>
2022-05-14 22:48:37 +0300

    #1

---
+++ Task_9_Exit_Cycle/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_9_Exit_Cycle
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите слово для выхода");
+
+            while (true)
+            {
+                string word = Console.ReadLine();
+                if (word != "exit")
+                {
+                    Console.WriteLine("Подумайте ещё");
+                }
+                else
+                {
+                    Console.WriteLine("Правильно! Вы вышли из цикла");
+                    break;
+                }
+            }
+        }
+    }
+}
